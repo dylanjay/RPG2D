2932eca614206c248b6fc44bc5432af713025730
darawl <dylanjay9@gmail.com>
2016-08-08 18:14:17 -0700

    move animations and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    [SerializeField]
+    private Transform topLeftAnchor;
+    [SerializeField]
+    private Transform bottomRightAnchor;
+    private Camera cam;
+
+    //float scaleFactor;
+    float lerpSpeed;
+
+    float verticalBound;
+    float horizontalBound;
+
+    [SerializeField]
+    private bool prewarm = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+        //scaleFactor = 1f;
+        lerpSpeed = 1f;
+
+        verticalBound = cam.orthographicSize;
+        horizontalBound = verticalBound * Screen.width / Screen.height;
+
+        if(prewarm)
+        {
+            CameraLerp(1);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //cam.orthographicSize = (Screen.height / 100f) / scaleFactor;
+        CameraLerp(lerpSpeed);
+    }
+
+    void CameraLerp(float lerpAmount)
+    {
+        if (target)
+        {
+            Vector3 camPos = target.position;
+
+            camPos.x = Mathf.Clamp(camPos.x, topLeftAnchor.position.x + horizontalBound, bottomRightAnchor.position.x - horizontalBound);
+            camPos.y = Mathf.Clamp(camPos.y, bottomRightAnchor.position.y + verticalBound, topLeftAnchor.position.y - verticalBound);
+
+            transform.position = Vector3.Lerp(transform.position, camPos, lerpAmount) + new Vector3(0, 0, -10);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float moveSpeed = 0f;
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if(moveX != 0 || moveY != 0)
+        {
+            anim.SetBool("Moving", true);
+        }
+
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
+
+        if(moveX < 0)
+        {
+            anim.SetInteger("Direction", 1);
+        }
+
+        else if(moveX > 0)
+        {
+            anim.SetInteger("Direction", 3);
+        }
+
+        else if(moveX == 0 && moveY > 0)
+        {
+            anim.SetInteger("Direction", 2);
+        }
+
+        else if(moveX == 0 && moveY < 0)
+        {
+            anim.SetInteger("Direction", 0);
+        }
+
+        anim.SetFloat("SpeedX", moveX);
+        anim.SetFloat("SpeedY", moveY);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * moveX, moveSpeed * moveY);
+    }
+}
