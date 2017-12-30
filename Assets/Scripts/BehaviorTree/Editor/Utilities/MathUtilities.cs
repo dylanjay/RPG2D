4c9e841093f983b9dde039fe936b1242739829fb
Mark De Ruyter <mderu001@ucr.edu>
2017-12-29 17:23:00 -0800

    Fixed improper math
    Also slightly increased speed on exit early case.

--- Assets/Scripts/BehaviorTree/Editor/Utilities/MathUtilities.cs
+++ Assets/Scripts/BehaviorTree/Editor/Utilities/MathUtilities.cs
@@ -16,17 +16,17 @@ namespace Benco.Utilities
         /// <param name="point">The point to check against for the line segment.</param>
         public static bool PointWithinLineSegment(Vector2 startPosition, Vector2 endPosition, float width, Vector2 point)
         {
-            Vector2 segment = startPosition - endPosition;
-            Vector2 relativePoint = startPosition - point;
-            Vector2 projection = relativePoint.ProjectOnto(segment);
-            float projectedDistance = projection.magnitude;
-            // If point not within the bounds below, exit early.
+            Vector2 segment = endPosition - startPosition;
+            Vector2 relativeSegment = endPosition - point;
+            float parameterizedProjection = Vector2.Dot(segment, relativeSegment);
+            // If point not within the bounds, exit early.
             //  |          |
             //  |--------->|
             //  |          |
-            if (projectedDistance < 0 || projectedDistance > segment.magnitude) { return false; }
+            if (parameterizedProjection < 0 || parameterizedProjection > segment.sqrMagnitude) { return false; }
 
-            Vector2 vectorRejection = relativePoint - projection;
+            Vector2 projection = relativeSegment.ProjectOnto(segment);
+            Vector2 vectorRejection = relativeSegment - projection;
             return vectorRejection.sqrMagnitude < width * width + Vector2.kEpsilon;
         }
     }
